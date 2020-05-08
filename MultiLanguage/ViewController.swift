7b61483d93926f043efd7e9166b0c2a29ef7577c
Alireza <aalirezamoradii@gmail.com>
2020-05-09 04:27:48 +0430

    Initial Commit

---
+++ MultiLanguage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiLanguage
+//
+//  Created by Alireza Moradi on 2/20/1399 AP.
+//  Copyright © 1399 Alireza Moradi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
